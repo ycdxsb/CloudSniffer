78db9b35b3bfad68c505a531d5642faacf454125
ycdxsb <1452740432@qq.com>
2020-03-03 20:21:20 +0800

    rename MainUI to MainWindow

--- MainWindow.py
+++ MainWindow.py
@@ -6,18 +6,16 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 
-class MainUI(QDialog):
+class MainWindow(QMainWindow):
     def __init__(self, parent=None):
-        super(MainUI, self).__init__(parent)
+        super(MainWindow, self).__init__(parent)
         self.setUpUI()
 
     def setUpUI(self):
-        self.resize(900, 600)
-        pass
-
+        self.resize(850,800)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mainUI = MainUI()
-    mainUI.show()
+    mainWindow = MainWindow()
+    mainWindow.show()
     sys.exit(app.exec_())
